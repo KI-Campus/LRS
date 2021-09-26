a9b86fa139c6373e8f1edcf638ce7a097be767b4
Ahmed Mujtaba <mujtabachang@gmail.com>
2021-09-27 03:01:53 +0500

    Return records to the frontend

--- app.js
+++ app.js
@@ -2,14 +2,17 @@
 require("rootpath")();
 const express = require("express");
 const app = express();
+const cors = require('cors');
 const jwt = require("_helpers/jwt");
 const errorHandler = require("_helpers/error-handler");
+
+app.use(cors({ origin: '*' }))
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json({ limit: "500mb" }));
 
 // Serve files in public folder
-app.use(express.static("public"));
+app.use(express.static("frontend/public"));
 
 // Use JWT auth to secure the API
 app.use(jwt());
@@ -32,16 +35,12 @@ const { MongoClient } = require("mongodb");
 var mongo_url = process.env.MONGO_URL || "mongodb://localhost:27017";
 const m_client = new MongoClient(mongo_url, { useUnifiedTopology: true });
 let m_database;
-let m_collection;
+
 
 // Async function to connect to MongoDB and initiaize variables for db and collection
 async function connectMongo() {
   try {
     await m_client.connect();
-    m_database = m_client.db(process.env.MONGO_DB || "lrs");
-    m_collection = m_database.collection(
-      process.env.MONGO_XAPI_COLLECTION || "records"
-    );
     console.log("Native MongoDB Driver connected");
   } catch (err) {
     console.log("Error connecting to Mongo Server: ", err);
@@ -53,8 +52,10 @@ connectMongo();
 app.post("/lrs", (req, res) => {
   try {
     // Encryption of personal data is moved to LTI Tool
-    m_collection.insertOne(req.body, function (err, mong_res) {
-      console.log(new Date(), " record inserted");
+    m_client.db(process.env.MONGO_DB || "lrs").collection(process.env.MONGO_XAPI_COLLECTION || "records").insertOne(req.body, { check_keys: false }, function (err, mong_res) {
+      if (err) { throw err }
+      console.log("xAPI Record inserted");
+      console.log("Mongo Response", mong_res)
       res.send(JSON.stringify({ result: "done" }));
       res.status(200).end();
     });
@@ -64,9 +65,40 @@ app.post("/lrs", (req, res) => {
   }
 });
 
+// Get records with find and sorting queries. Also have pagination support (limit and skip)
+app.get("/records/get", (req, res) => {
+  let query;
+  let sort;
+  let limit;
+  let skip;
+  try {
+    req.body.query ? query = req.body.query : query = {};
+    req.body.sort ? sort = req.body.sort : sort = {};
+    req.body.limit ? limit = req.body.limit : 0;
+    req.body.skip ? limit = req.body.skip : 0;
+    m_client.db(process.env.MONGO_DB || "lrs").collection(process.env.MONGO_XAPI_COLLECTION || "records").find(query, { limit: limit, skip: skip, sort: sort }).toArray(function (err, results) {
+      if (err) throw err;
+      console.log("Records request received", req.body);
+      res.status(200).send({ total: results.length, results: results }).end();
+    });
+
+  }
+  catch (err) {
+    console.log("Error while getting records", err);
+    console.log("Input Query was: ", query);
+    console.log("Sort Query was: ", sort);
+    console.log("Limit: ", limit);
+    console.log("Skip: ", skip);
+    res.status(500).end();
+  }
+});
+
+
 // Start the Server
 const port =
   process.env.NODE_ENV === "production" ? process.env.PORT || 80 : 4000;
-const server = app.listen(port, function () {
+app.listen(port, function () {
   console.log("Server listening on port " + port);
 });
+
+
