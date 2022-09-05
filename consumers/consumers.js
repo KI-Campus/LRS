5aa97f01f8dd9ba501176435fb05b8e417262dea
Ahmed Mujtaba Chang <mujtabachang@gmail.com>
2022-09-05 22:52:22 +0500

    Consumers are sorted via title/name

--- consumers/consumers.js
+++ consumers/consumers.js
@@ -3,8 +3,8 @@ const jwtAuthz = require("express-jwt-authz");
 const router = express.Router();
 
 let jwtScopeOptions = {
-    failWithError: true,
-    customScopeKey: "role",
+  failWithError: true,
+  customScopeKey: "role",
 };
 
 var m_client;
@@ -23,177 +23,228 @@ router.delete("/:id", jwtAuthz(["admin"], jwtScopeOptions), _delete);
 
 module.exports = { router, init };
 
-
 function init(mongoClient) {
-    m_client = mongoClient;
+  m_client = mongoClient;
 }
-
 
 function register(req, res, next) {
+  // If consumer id is "all" then show error
+  if (req.body.id && req.body.id.toLowerCase() === "all") {
+    res
+      .status(400)
+      .send({ success: false, message: "Consumer ID cannot be 'all'" });
+    return;
+  }
 
-    // If consumer id is "all" then show error
-    if (req.body.id && req.body.id.toLowerCase() === "all") {
-        res.status(400).send({ success: false, message: "Consumer ID cannot be 'all'" });
-        return;
-    }
+  if (
+    req.body.id &&
+    req.body.id.length > 0 &&
+    req.body.name &&
+    req.body.name.length > 0
+  ) {
+    req.body.id = req.body.id.toLowerCase();
+    req.body.createdAt = new Date();
 
-    if (req.body.id && req.body.id.length > 0 && req.body.name && req.body.name.length > 0) {
-        req.body.id = req.body.id.toLowerCase();
-        req.body.createdAt = new Date();
-
-        // Find if any record exists with the same id
-        m_client.db().collection("consumers").findOne({ id: req.body.id }, (err, result) => {
-            if (err) {
+    // Find if any record exists with the same id
+    m_client
+      .db()
+      .collection("consumers")
+      .findOne({ id: req.body.id }, (err, result) => {
+        if (err) {
+          console.log(err);
+          res.status(500).send({
+            success: false,
+            message: "Error while registering consumer",
+          });
+        } else if (result) {
+          res
+            .status(409)
+            .send({ success: false, message: "Consumer already exists" });
+        } else {
+          m_client
+            .db()
+            .collection("consumers")
+            .insertOne(req.body, (err, result) => {
+              if (err) {
                 console.log(err);
-                res.status(500).send({ success: false, message: "Error while registering consumer" });
-            } else if (result) {
-                res.status(409).send({ success: false, message: "Consumer already exists" });
-            } else {
-                m_client.db().collection("consumers").insertOne(req.body, (err, result) => {
-                    if (err) {
-                        console.log(err);
-                        res.status(500).send(err);
-                    } else {
-                        res.status(200).send({ success: true, message: "Consumer created", result: result });
-                    }
+                res.status(500).send(err);
+              } else {
+                res.status(200).send({
+                  success: true,
+                  message: "Consumer created",
+                  result: result,
                 });
-            }
-        });
-
-    }
-    else {
-        res.status(400).send({ success: false, message: "Invalid request" });
-    }
+              }
+            });
+        }
+      });
+  } else {
+    res.status(400).send({ success: false, message: "Invalid request" });
+  }
 }
-
 
 function getAll(req, res, next) {
-
-    if (req.user.role === "admin") {
-        // Read all consumers
-        m_client.db().collection("consumers").find({}).toArray((err, result) => {
-            if (err) {
-                console.log("Error reading all consumers for admin", err);
-                res.status(500).send({ success: false, message: "Error while getting consumers" });
-            } else {
-                // Admin has access to all
-                result.push({ id: "all", name: "All" });
-                res.status(200).send({ success: true, result: result });
-            }
-        });
-    }
-    // Return only the consumers which this user has access to
-    else {
-        // Get the list of consumersAccess field of current user
-        m_client.db().collection("users").findOne({ email: req.user.email }, (err, resultUser) => {
-            if (err) {
-                console.log("Error reading all consumers for user", err);
-                res.status(500).send({ success: false, message: "Error while getting consumers" });
-            } else if (resultUser) {
-
-                // Read all consumers
-                m_client.db().collection("consumers").find({ id: { $in: resultUser.consumersAccess } }).toArray((err, result) => {
-                    if (err) {
-                        console.log(err);
-                        res.status(500).send({ success: false, message: "Error while getting consumers" });
-                    } else {
-                        if (resultUser.consumersAccess.indexOf("all") > -1) {
-                            result.push({ id: "all", name: "All" });
-                        }
-                        res.status(200).send({ success: true, result: result });
-                    }
+  if (req.user.role === "admin") {
+    // Read all consumers
+    m_client
+      .db()
+      .collection("consumers")
+      .find({})
+      .sort({ name: 1 })
+      .toArray((err, result) => {
+        if (err) {
+          console.log("Error reading all consumers for admin", err);
+          res
+            .status(500)
+            .send({ success: false, message: "Error while getting consumers" });
+        } else {
+          // Admin has access to all
+          result.push({ id: "all", name: "All" });
+          res.status(200).send({ success: true, result: result });
+        }
+      });
+  }
+  // Return only the consumers which this user has access to
+  else {
+    // Get the list of consumersAccess field of current user
+    m_client
+      .db()
+      .collection("users")
+      .findOne({ email: req.user.email }, (err, resultUser) => {
+        if (err) {
+          console.log("Error reading all consumers for user", err);
+          res
+            .status(500)
+            .send({ success: false, message: "Error while getting consumers" });
+        } else if (resultUser) {
+          // Read all consumers
+          m_client
+            .db()
+            .collection("consumers")
+            .find({ id: { $in: resultUser.consumersAccess } })
+            .sort({ name: 1 })
+            .toArray((err, result) => {
+              if (err) {
+                console.log(err);
+                res.status(500).send({
+                  success: false,
+                  message: "Error while getting consumers",
                 });
-
-            } else {
-                res.status(404).send({ success: false, message: "User not found" });
-            }
-        });
-
-
-    }
+              } else {
+                if (resultUser.consumersAccess.indexOf("all") > -1) {
+                  result.push({ id: "all", name: "All" });
+                }
+                res.status(200).send({ success: true, result: result });
+              }
+            });
+        } else {
+          res.status(404).send({ success: false, message: "User not found" });
+        }
+      });
+  }
 }
 
-
-
 function getById(req, res, next) {
-    // Read consumer by id
-    // Check if id is not empty and if it exists
-    if (req.params.id && req.params.id.length > 0) {
-
-        m_client.db().collection("consumers").findOne({ id: req.params.id }, (err, result) => {
-            if (err) {
-                console.log(err);
-                res.status(500).send({ success: false, message: "Error while getting consumer" });
-            } else if (result) {
-                res.status(200).send({ success: true, result: result });
-            } else {
-                res.status(404).send({ success: false, message: "Consumer not found" });
-            }
-        });
-    }
-    else {
-        res.status(400).send({ success: false, message: "Invalid request" });
-    }
+  // Read consumer by id
+  // Check if id is not empty and if it exists
+  if (req.params.id && req.params.id.length > 0) {
+    m_client
+      .db()
+      .collection("consumers")
+      .findOne({ id: req.params.id }, (err, result) => {
+        if (err) {
+          console.log(err);
+          res
+            .status(500)
+            .send({ success: false, message: "Error while getting consumer" });
+        } else if (result) {
+          res.status(200).send({ success: true, result: result });
+        } else {
+          res
+            .status(404)
+            .send({ success: false, message: "Consumer not found" });
+        }
+      });
+  } else {
+    res.status(400).send({ success: false, message: "Invalid request" });
+  }
 }
 
 function update(req, res, next) {
-    // Update consumer by id
+  // Update consumer by id
 
-    // If consumer id is "all" then show error
-    if (req.params.id && req.params.id.toLowerCase() === "all") {
-        res.status(400).send({ success: false, message: "Consumer id cannot be 'all'" });
-        return;
-    }
+  // If consumer id is "all" then show error
+  if (req.params.id && req.params.id.toLowerCase() === "all") {
+    res
+      .status(400)
+      .send({ success: false, message: "Consumer id cannot be 'all'" });
+    return;
+  }
 
-    // Check if id is not empty and if it exists
-    if (req.params.id && req.params.id.length > 0) {
-
-        m_client.db().collection("consumers").updateOne({ id: req.params.id }, { $set: req.body }, (err, result) => {
-            if (err) {
-                console.log(err);
-                res.status(500).send({ success: false, message: "Error while updating consumer" });
-            } else if (result) {
-                res.status(200).send({ success: true, result: result });
-            } else {
-                res.status(404).send({ success: false, message: "Consumer not found" });
-            }
-        });
-    }
-    else {
-        res.status(400).send({ success: false, message: "Invalid request" });
-    }
-
+  // Check if id is not empty and if it exists
+  if (req.params.id && req.params.id.length > 0) {
+    m_client
+      .db()
+      .collection("consumers")
+      .updateOne({ id: req.params.id }, { $set: req.body }, (err, result) => {
+        if (err) {
+          console.log(err);
+          res
+            .status(500)
+            .send({ success: false, message: "Error while updating consumer" });
+        } else if (result) {
+          res.status(200).send({ success: true, result: result });
+        } else {
+          res
+            .status(404)
+            .send({ success: false, message: "Consumer not found" });
+        }
+      });
+  } else {
+    res.status(400).send({ success: false, message: "Invalid request" });
+  }
 }
 
-
 function _delete(req, res, next) {
-    // Delete consumer by id
-    // Check if id is not empty and if it exists
-    if (req.params.id && req.params.id.length > 0) {
+  // Delete consumer by id
+  // Check if id is not empty and if it exists
+  if (req.params.id && req.params.id.length > 0) {
+    // Check if consumer id is found in the database
+    m_client
+      .db()
+      .collection("consumers")
+      .findOne({ id: req.params.id }, (err, result) => {
+        if (err) {
+          console.log(err);
+          res
+            .status(500)
+            .send({ success: false, message: "Error while deleting consumer" });
+        } else if (result) {
+          // Delete consumer
 
-        // Check if consumer id is found in the database
-        m_client.db().collection("consumers").findOne({ id: req.params.id }, (err, result) => {
-            if (err) {
+          m_client
+            .db()
+            .collection("consumers")
+            .deleteOne({ id: req.params.id }, (err, result) => {
+              if (err) {
                 console.log(err);
-                res.status(500).send({ success: false, message: "Error while deleting consumer" });
-            } else if (result) {
-
-                // Delete consumer
-
-                m_client.db().collection("consumers").deleteOne({ id: req.params.id }, (err, result) => {
-                    if (err) {
-                        console.log(err);
-                        res.status(500).send({ success: false, message: "Error while deleting consumer" });
-                    } else if (result) {
-                        res.status(200).send({ success: true, result: result });
-                    } else {
-                        res.status(404).send({ success: false, message: "Consumer not found" });
-                    }
+                res.status(500).send({
+                  success: false,
+                  message: "Error while deleting consumer",
                 });
-            } else {
-                res.status(404).send({ success: false, message: "Consumer not found" });
-            }
+              } else if (result) {
+                res.status(200).send({ success: true, result: result });
+              } else {
+                res
+                  .status(404)
+                  .send({ success: false, message: "Consumer not found" });
+              }
+            });
+        } else {
+          res
+            .status(404)
+            .send({ success: false, message: "Consumer not found" });
         }
-        );
-    }
+      });
+  }
 }
